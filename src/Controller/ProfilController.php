1a5b9d8430374d68765f95d092797667c70ea701
Fabien CURFS <84622048+FabhinoCy@users.noreply.github.com>
2024-12-27 21:31:47 +0100

    Formulaire update user

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,15 +2,42 @@
 
 namespace App\Controller;
 
+use App\Form\UserEditFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ProfilController extends AbstractController
 {
+    /**
+     * @throws \Exception
+     */
     #[Route('/profil', name: 'app_profil')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('profil/index.html.twig');
+        $user = $this->getUser();
+        $form = $this->createForm(UserEditFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $request->request->all()['user_edit_form'];
+
+
+            $user->setPseudo($data['pseudo']);
+            $user->setLastname($data['lastname']);
+            $user->setFirstname($data['firstname']);
+            $user->setBirthday(new \DateTimeImmutable($data['birthday']));
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('profil/index.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
